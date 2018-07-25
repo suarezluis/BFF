b1a8e8bdab2836c01418e549bcdb98d13b7a9ac2
Luis Suarez <suarezluis@gmail.com>
2018-07-25 02:01:42 -0500

    added secretOrKey and function to return web token

--- config/keys.example.js
+++ config/keys.example.js
@@ -1,4 +1,5 @@
 // Fill up information and rename to keys.js
 module.exports = {
-  mongoURI: "mongodb://username:password@ds137687.mlab.com:37687/bff-db"
+  mongoURI: "mongodb://username:password@ds137687.mlab.com:37687/bff-db",
+  secretOrKey: "secret"
 };
