1d7a3e09e641eb1e1f75d296f056970989fc73f9
Luis Suarez <suarezluis@gmail.com>
2018-07-26 01:57:54 -0500

    added name validation and fixed spelling bugs

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken"); // JSON Web Token
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load input validation
+const validateRegisterInput = require("../../validation/register");
+
 // Load User model
 const User = require("../../models/User");
 
@@ -18,8 +21,16 @@ router.get("/test", (req, res) => res.json({ msg: "🍺 Users Works ✔" }));
 // @description Register user
 // @access Public
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body); //Destructuring
+
+  // Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
+      errors.email = "Email already exists";
       return res.status(400).json({ email: "Email already exists" });
     } else {
       const avatar = gravatar.url(req.body.email, {
